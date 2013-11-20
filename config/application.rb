84a940a477c99d83cd0ae35452c2ec04605fe7b9
Felippe <raposo.felippe@gmail.com>
2013-11-20 03:55:50 -0200

    Customize default generators

--- config/application.rb
+++ config/application.rb
@@ -13,6 +13,16 @@ Bundler.require(:default, Rails.env)
 
 module Rails4DeviseRspecCucumber
   class Application < Rails::Application
+
+    config.generators do |g|
+      g.test_framework :rspec, fixture: true
+      g.fixture_replacement :factory_girl, dir: 'spec/factories'
+      g.view_specs false
+      g.helper_specs false
+      g.stylesheets = false
+      g.javascripts = false
+      g.helper = false
+    end
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
