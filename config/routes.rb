6b525803f4e41210c42a75c4734cfc88f02f2839
Felippe <raposo.felippe@gmail.com>
2013-11-20 15:26:54 -0200

    Adds root route to home_controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails4DeviseRspecCucumber::Application.routes.draw do
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
-
+  
+  root 'home#index'
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
 
