64c53f7a10d87098c624496fd255816d5534f966
Felippe <raposo.felippe@gmail.com>
2013-11-22 13:59:14 -0200

    Adds some devise configuration

--- config/application.rb
+++ config/application.rb
@@ -23,6 +23,7 @@ module Rails4DeviseRspecCucumber
       g.javascripts = false
       g.helper = false
     end
+    config.filter_parameters += [:password, :password_confirmation]
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
