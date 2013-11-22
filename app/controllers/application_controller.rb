526656562fcc5b3ecbb7a66b3333fe1ea1ec79c6
Felippe <raposo.felippe@gmail.com>
2013-11-22 16:33:09 -0200

    Adds protected to sanitizaze_params

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,7 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_filter :update_sanitized_params, if: :devise_controller?
 
+  protected
   def update_sanitized_params
       devise_parameter_sanitizer.for(:sign_up) {|u| u.permit(:name,:email, :password, :password_confirmation)}
   end
