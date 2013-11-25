7da281a37ecb9f786cf1adf492f9cb5817038638
Felippe <raposo.felippe@gmail.com>
2013-11-25 15:00:38 -0200

    Adds a current_user helper, on application controller.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,4 +9,10 @@ class ApplicationController < ActionController::Base
       devise_parameter_sanitizer.for(:sign_up) {|u| u.permit(:name,:email, :password, :password_confirmation)}
   end
 
+  def current_user
+    @current_user ||= User.find(session[:user_id]) if session[:user_id]
+  end
+
+  helper_method :current_user
+
 end
