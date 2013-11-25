aaa78bbf7d8141799ced1077526df61617b08bd6
Felippe <raposo.felippe@gmail.com>
2013-11-25 20:39:22 -0200

    Facebook login is ready!

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,3 @@
 # Be sure to restart your server when you modify this file.
 
-Rails4DeviseRspecCucumber::Application.config.session_store :cookie_store, key: '_rails4-devise-rspec-cucumber_session'
+Rails4DeviseRspecCucumber::Application.config.session_store :active_record_store
