81b7671426477d33ac9ef87ccb2e20b68f749b75
Felippe <raposo.felippe@gmail.com>
2013-11-25 14:58:32 -0200

    Adds omniauthable fields to User

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131122174223) do
+ActiveRecord::Schema.define(version: 20131125164424) do
 
   create_table "users", force: true do |t|
     t.string   "email",                  default: "", null: false
@@ -27,6 +27,10 @@ ActiveRecord::Schema.define(version: 20131122174223) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "name"
+    t.string   "provider"
+    t.string   "uid"
+    t.string   "oauth_token"
+    t.datetime "oauth_expires_at"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
