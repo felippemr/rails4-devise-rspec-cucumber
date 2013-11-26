df3f78b2df1b2bc97be55ca00a4fde3359cdb569
Felippe <raposo.felippe@gmail.com>
2013-11-26 20:37:50 -0200

    Fix some stuff

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -19,7 +19,7 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
       flash[:notice] = I18n.t "devise.omniauth_callbacks.success", :kind => "Google"
       sign_in_and_redirect @user, :event => :auththentication
     else
-      session["devise.google_data"] = request.eventnv["omniauth.auth"]
+      session["devise.google_data"] = request.env["omniauth.auth"]
       redirect_to new_user_registration_url
     end
   end
